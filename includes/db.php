52627fd14d0e802f638a97bef2cc367f6ef81876
Nikeeta <nikeetadhungel@gmail.com>
2025-04-20 04:04:01 +0530

    Add Docker Compose configuration

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "db"; // Docker service name, NOT 'localhost'
-$username = "root";
-$password = "root";
-$dbname = "lost_and_found";
+// Database credentials
+$servername = 'mysql'; // Use the service name from docker-compose.yml
+$username = 'root';
+$password = 'root';  // Or your MySQL root password
+$dbname = 'lost_and_found';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
